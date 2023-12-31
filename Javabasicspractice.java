4ee08ea158eff722c02bbc4b0021e82311cf2842
MohammadAsiya <Asiya1mohammad@gmail.com>
2023-12-31 20:05:24 +0530

    Day-1

---
+++ Javabasicspractice.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Javabasicspractice{
+    public static void main (String [] args){
+    //sum of two elements
+   /*Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int total = a + b;
+    System.out.println(total);
+   */
+
+   //product of two elements
+   /*Scanner sc =new Scanner(System.in);
+   int a = sc.nextInt();
+   int b = sc.nextInt();
+   int product =a * b;
+   System.out.println(product);*/
+
+   //Area of circle
+   Scanner sc= new Scanner(System.in);
+   Float rad = sc.nextFloat();
+   Float area =3.14f*rad*rad;
+   System.out.println(area);
+
+    }
+}

8b26e8df6a1e6f77cb65a689074f23fe5295c2c2
ASIYA MOHAMMAD <121110715+MohammadAsiya@users.noreply.github.com>
2024-01-12 16:40:40 +0530

    12/1/24

--- pattern.java
+++ pattern.java
@@ -50,7 +50,131 @@ public class pattern {
             }
             System.out.println();
 
-        */    
+        */   
+        
+        
+        //Inverted rotated half pyramid
+        /* *
+          **
+         ***
+        ****
+       ***** */
+       /* Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+
+
+        //Inverted half pyramid numbers pattern
+       /* Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int count=1;
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+        */
+
+
+        //Floyd's Triangle
+        /*Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+        int count=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }*/
+
+
+        //0-1 Triangle
+        /*Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }*/
+
+
+        //Solid rhombous pattern
+       /* Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+
+        //Hollow Rohmbus
+       /* Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        */
+
+
+        //Diamond pattern
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         }
 }
 

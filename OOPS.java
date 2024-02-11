e8cec5a190821976a047d099450e93b8ecc5d9a7
ASIYA MOHAMMAD <121110715+MohammadAsiya@users.noreply.github.com>
2024-02-11 22:31:33 +0530

    OOPS.java

--- OOPS.java
+++ OOPS.java
@@ -1,4 +1,6 @@
 
+import javax.swing.plaf.synth.SynthScrollPaneUI;
+import javax.xml.transform.stax.StAXResult;
 import javax.xml.transform.stax.StAXSource;
 
 import javafx.scene.layout.StackPane;
@@ -127,31 +129,252 @@ class Student {
 
 //==>Copy constructor
 //It's work is to copy one object properties to another object.
-  public static void main(String args[]){
+    /*public static void main(String args[]){
         Student s1 = new Student();
-        s1.name ="Asiya";
-        s1.roll=79;
-        s1.password="abcd";
+        s1.name="Asiya";
+        s1.roll = 79;
+        s1.password ="abcd";
+
+
         s1.marks[0]=100;
         s1.marks[1]=90;
-        s1.marks[2]=80; 
+        s1.marks[2]=80;
+        Student s2 = new Student(s1);//copying of s1 values into s2.
+        s2.password ="xyz";
+        s1.marks[2]=100;//Here we are changing s1 marks In above code s2 is already copied s1 marks
+        //but now it is changed .Because s2 didn't store the array(it stores array reference) it is pointing to s1 's array. 
 
-        //Student s2 = new Student(s1);
-        /*s2.password="xyz";  
         for(int i=0;i<3;i++){
             System.out.println(s2.marks[i]);
-        } */   
-    }
+        }
+        
+  }
 }
 class Student{
     String name;
     int roll;
-    int marks[];
     String password;
+    int marks[];
+///==> Shallow  Copy constructor
+    //Student(Student s1){
+    //    marks = new int[3];
+    //    this.name=s1.name;
+    //    this.roll =s1.roll;
+    //    this.marks=s1.marks;
+    //}
+
+    //Deep copy
+
     /*Student(Student s1){
-        marks = new int[3];
-        this.name =s1.name;
-        this.roll =s1.roll;
-        this.marks=s1.marks;
+       marks = new int[3];
+       this.name=s1.name;
+       this.roll =s1.roll;
+       for(int i=0;i<marks.length;i++){
+        this.marks[i]=s1.marks[i];
+       }
     }*/
+    /*Student(){
+        marks = new int[3];
+        System.out.println("Constructor is called...");
+    }
+    Student(String name){
+        marks = new int[3];
+        this.name = name;
+    }
+    Student(int roll){
+        marks = new int[3];
+        this.roll =roll;
+    } */
+
+
+//==>Shallow copy and deep copy differences
+//In shallow copy changes will be reflected[ex.array]
+//In Deep copy changes will not be reflected.
+
+//Lazy copy
+
+
+//==>Destructor .In java Garbage collector auatomatically destruct unneccesary elements.
+
+
+//####==>Inheritance
+//Inheritance is when properties and methods of base class are passed onto the derived class.(like parents DNA comes to us)
+//Base class or parent class gives it's properties to the derived class or child class.
+
+
+//==>Inheritance
+
+/* 
+//Single inheritanc
+    public static void main(String args[]){
+        Fish shark = new Fish();
+        shark.eat();
+        shark.breathe();
+        shark.swim();
+    }
 }
+//Base class
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("Eating..");
+    }
+    void breathe() {
+        System.out.println("breeathing..");
+    }
+}
+
+//Derived class
+class Fish extends Animal{
+    int fins;
+    void swim(){
+        System.out.println("swims in water..~~~");
+    }
+}
+*/
+
+//Multi level inheritance.
+/*
+  public static void main(String args[]){
+    //Multilevel inheritance
+    dog puppy =new dog();
+    puppy.legs=4;
+    System.out.println(puppy.legs);
+    puppy.bread ="white hair";
+    System.out.println(puppy.bread);
+    puppy.breathe();
+
+  }
+}
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eating");
+    }
+    void breathe(){
+        System.out.println("breethe");
+    }
+}
+class Mammal extends Animal{
+    int legs;
+}
+class dog extends Mammal{
+    String bread;
+}*/
+
+
+
+//==>Heirarical inheritance
+/*
+    public static void main(String args[]){
+        Fish shark = new Fish();
+        shark.eat();
+        shark.breeth();
+        shark.swim();
+        Mammal mam = new Mammal();
+        mam.eat();
+        mam.breeth();
+        mam.walk();
+        bird dove = new bird();
+        dove.eat();
+        dove.breeth();
+        dove.fly();
+    }
+}
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eating");
+    }
+    void breeth(){
+        System.out.println("breethe");
+    }
+}
+class Mammal extends Animal{
+    void walk(){
+        System.out.println("Walking");
+    }
+}
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swimming in water~~");
+    }
+}
+class bird extends Animal{
+    void fly(){
+        System.out.println("Flying");
+    }
+}*/
+
+//Hybrid inheritance
+//Homework problem
+    public static void main(String[] args) {
+        human h = new human();
+        h.spl();
+        h.walk();
+        h.breethe();
+  }
+}
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eating food");
+    }
+    void breethe(){
+        System.out.println("breething");
+    }
+}
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swimming in water");
+    }
+}
+class Tuna extends Fish{
+    void speacial(){
+        System.out.println("looks beautiful");
+    }
+}
+class shark extends Fish{
+    void spl(){
+        System.out.println("Can eat humans");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("Flying in sky");
+    }
+}
+class peakok extends Bird{
+    void spl(){
+        System.out.println("can dance well");
+    }
+}
+class Mammal extends Animal{
+    void walk(){
+        System.out.println("walking on earth");
+    }
+}
+class dog extends Mammal{
+    void love(){
+        System.out.println("Love Bones");
+    }
+}
+class cat extends Mammal{
+    void love(){
+        System.out.println("Love milk");
+    }
+}
+class human extends Mammal{
+    void spl(){
+        System.out.println("Intelligent");
+    }
+}
+
+
+
+//TYPES OF INHERITANCE-4
+//1)Single level inheritance      base class----->>>derived class
+//2)Multi level inheritance       base class----->>>>derived class------>>>>derived class
+//3)Heirarical inheritance     one base class =====>>>Multiple derived classes.
+//4)Hybrid inheritance
+//5)Multiple inheritance is not available in java.
